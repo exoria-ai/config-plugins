c1fa4d8092fe11ec7d0e66a8a7e3043288c6a171
Ryan Pream <ryan@aimachinedream.com>
2025-05-20 16:14:29 -0700

    feat: create standalone plugin that doesn't require build step

--- packages/react-native-webrtc/app.plugin.js
+++ packages/react-native-webrtc/app.plugin.js
@@ -1,1 +1,69 @@
-module.exports = require("./build/withWebRTC");
+const { AndroidConfig, withInfoPlist, createRunOncePlugin, WarningAggregator } = require("expo/config-plugins");
+
+const CAMERA_USAGE = "Allow $(PRODUCT_NAME) to access your camera";
+const MICROPHONE_USAGE = "Allow $(PRODUCT_NAME) to access your microphone";
+
+const withBitcodeDisabled = (config) => {
+	if (!config.ios) {
+		config.ios = {};
+	}
+
+	if (config.ios?.bitcode != null && config.ios?.bitcode !== false) {
+		WarningAggregator.addWarningIOS(
+			"ios.bitcode",
+			"react-native-webrtc plugin is overwriting project bitcode settings. WebRTC requires bitcode to be disabled for builds, targeting physical iOS devices.",
+		);
+	}
+	// WebRTC requires Bitcode be disabled for
+	// production AND development iOS builds that target devices, e.g. not simulators.
+	// SDK +44 property
+	config.ios.bitcode = false;
+	return config;
+};
+
+const withPermissions = (config, props) => {
+	return withInfoPlist(config, (config) => {
+		const { cameraPermission, microphonePermission } = props || {};
+
+		config.modResults.NSCameraUsageDescription =
+			cameraPermission ||
+			config.modResults.NSCameraUsageDescription ||
+			CAMERA_USAGE;
+
+		config.modResults.NSMicrophoneUsageDescription =
+			microphonePermission ||
+			config.modResults.NSMicrophoneUsageDescription ||
+			MICROPHONE_USAGE;
+
+		return config;
+	});
+};
+
+const withWebRTC = (config, props = {}) => {
+	const _props = props || {};
+
+	// iOS
+	config = withPermissions(config, _props);
+	config = withBitcodeDisabled(config);
+
+	// Android
+	// https://github.com/react-native-webrtc/react-native-webrtc/blob/master/Documentation/AndroidInstallation.md#declaring-permissions
+	config = AndroidConfig.Permissions.withPermissions(config, [
+		"android.permission.ACCESS_NETWORK_STATE",
+		"android.permission.CAMERA",
+		"android.permission.INTERNET",
+		"android.permission.MODIFY_AUDIO_SETTINGS",
+		"android.permission.RECORD_AUDIO",
+		"android.permission.SYSTEM_ALERT_WINDOW",
+		"android.permission.WAKE_LOCK",
+		"android.permission.BLUETOOTH",
+	]);
+
+	return config;
+};
+
+module.exports = createRunOncePlugin(
+	withWebRTC,
+	"react-native-webrtc",
+	"UNVERSIONED"
+);
